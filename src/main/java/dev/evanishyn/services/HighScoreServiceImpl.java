9d4ab8456e23c7c6a1bea245e92c92a549e883b8
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-08-30 15:35:40 -0700

    Implemented CreateNewScore logic in services

--- src/main/java/dev/evanishyn/services/HighScoreServiceImpl.java
+++ src/main/java/dev/evanishyn/services/HighScoreServiceImpl.java
@@ -17,8 +17,21 @@ public class HighScoreServiceImpl implements HighScoreService {
 
     @Override
     public HighScore createNewScore(HighScore highScore) {
-        HighScore hs = this.highScoreRepo.save(highScore);
-        return hs;
+        if(highScore.getInitials() != null){
+            String updInitials = highScore.getInitials().toUpperCase();
+
+            if(updInitials.length()>3 ){
+                updInitials = highScore.getInitials().substring(0,2).toUpperCase();
+                highScore.setInitials(updInitials);
+
+                if(highScore.getHighScore() < 0 && highScore.getInitials().length() == 3){
+                return this.highScoreRepo.save(highScore);
+                }
+                throw new RuntimeException("Must have a score above 0 and 3 letters of initials");
+            }
+            throw new RuntimeException("You need at least 3 characters for your initials");
+        }
+        throw new RuntimeException("Initials required. (Preferably 3 characters and uppercase)");
     }
     //no negative scores
     //initials only 3 characters long
@@ -28,9 +41,13 @@ public class HighScoreServiceImpl implements HighScoreService {
     @Override
     public HighScore getScoreById(int id) {
         Optional<HighScore> possibleHighscore = this.highScoreRepo.findById(id);
-        return null;
-    }
 
+        if(possibleHighscore.isPresent()){
+            return possibleHighscore.get();
+        }else{
+            throw new RuntimeException("Id not found");
+        }
+    }
 
     @Override
     public List<HighScore> getAllScores() {
