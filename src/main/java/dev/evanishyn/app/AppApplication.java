6f19a3e866c6cc06caa780822909a26c1f7c36a5
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-08-30 13:54:31 -0700

    Added annotations to appApplication. Tests = pass

--- src/main/java/dev/evanishyn/app/AppApplication.java
+++ src/main/java/dev/evanishyn/app/AppApplication.java
@@ -2,8 +2,14 @@ package dev.evanishyn.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dev.evanishyn"})
+@EntityScan(basePackages = "dev.evanishyn.entities")
+@EnableJpaRepositories(basePackages = {"dev.evanishyn.repos"})
 public class AppApplication {
 
 	public static void main(String[] args) {
